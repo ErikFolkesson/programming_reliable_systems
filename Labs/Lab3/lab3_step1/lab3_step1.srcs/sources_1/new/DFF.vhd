7f954a89e2bc9a12d07822f7c5eb881cea4bce0a
Erik Folkesson <erik@familjenfolkesson.se>
2025-02-11 15:39:26 +0100

    Finished lab 2 - Started work on lab 3

---
+++ Labs/Lab3/lab3_step1/lab3_step1.srcs/sources_1/new/DFF.vhd
@@ -0,0 +1,46 @@
+library ieee ;
+use ieee.std_logic_1164.All;
+use IEEE.numeric_std.all;
+
+
+entity Dflipflop is
+    port (D      :in  std_logic;
+          clk    :in  std_logic;
+
+          Q      :out std_logic:='0'
+    );
+end Dflipflop;
+
+
+architecture Behavioral of Dflipflop is
+
+    begin
+        process (clk)
+        begin
+            if (clk'event and clk='1') then
+                Q <= D;
+            end if;
+        end process;
+end Behavioral;
+
+library ieee;
+use ieee.std_logic_1164.All;
+use IEEE.numeric_std.all;
+
+entity tb_Dflipflop is end;
+
+architecture tb_Dflipflop_arch of tb_Dflipflop is
+    signal D     :std_logic := '0';
+    signal clk   :std_logic := '0';
+
+    signal Q     :std_logic;
+
+begin
+    Dflipflop_entity: entity work.Dflipflop
+    port map (D, clk, Q);
+    
+    clk <= not clk after 10ps;
+
+    D <= not D after 15ps;
+
+end tb_Dflipflop_arch;
